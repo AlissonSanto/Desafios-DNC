09ee4bea792f66ab8890554488cfb9f339797d2b
AlissonSanto <alissonsantos6150@gmail.com>
2023-07-02 22:46:43 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,6 @@
+var n850 = window.document.getElementById(n850)
+var n40 = window.document.getElementById(n40)
+var n2000000 = window.document.getElementById(n2000000)
+
+function ContarAte850()
+     
